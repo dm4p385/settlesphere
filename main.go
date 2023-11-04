009487c6768f63289c884b120995215311e8ac10
Harsh Singh <harshsingh050803@gmail.com>
2023-11-04 18:26:31 +0530

    feat: add CORS config

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	recover2 "github.com/gofiber/fiber/v2/middleware/recover"
 	"settlesphere/config"
 	"settlesphere/db"
@@ -15,6 +16,12 @@ func main() {
 		ServerHeader: "SettleSphere",
 		AppName:      "SettleSphere",
 	})
+	fiberApp.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 	app := config.InitializeApp(fiberApp, entClient)
 	fiberApp.Use(recover2.New())
 	// setup routes
