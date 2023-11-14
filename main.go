cc692ebccd28d82807c2c1eb561d84ee54a1bf31
Harsh Singh <43901843+DankMemes4President@users.noreply.github.com>
2023-11-14 19:06:10 +0530

    fix: add Authorization to allowed headers in CORS

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 		AppName:      "SettleSphere",
 	})
 	fiberApp.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin,Authorization",
 		AllowOrigins:     "*",
 		AllowCredentials: true,
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
